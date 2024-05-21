b80cfafc157ac2d1dac715f5ed08950c94227f8d
rifqipr <m.rifqi.p80@gmail.com>
2024-05-21 23:08:47 +0700

    working infra-1

---
+++ provider.tf
@@ -0,0 +1,16 @@
+provider "google" {
+  project = "ta-chetbot"
+  region = "us-central1"
+}
+terraform {
+  backend "gcs" {
+    bucket = "ta-chetbot-bucket"
+    prefix = "terraform/state"
+  }
+  required_providers {
+    google = {
+        source = "hashicorp/google"
+        version = "~> 4.0"
+    }
+  }
+}
